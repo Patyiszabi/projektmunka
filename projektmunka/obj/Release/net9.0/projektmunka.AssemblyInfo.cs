352f8a562351e61559aa7349580f4ff6f4d0beef
Patyiszabi <patyi.szabolcs@diak.szbi-pg.hu>
2025-02-26 18:04:02 +0100

    .

--- projektmunka/obj/Release/net9.0/projektmunka.AssemblyInfo.cs
+++ projektmunka/obj/Release/net9.0/projektmunka.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("projektmunka")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a5ecde2fc78d8d1fd1535a9cd7c1aac3944f558")]
 [assembly: System.Reflection.AssemblyProductAttribute("projektmunka")]
